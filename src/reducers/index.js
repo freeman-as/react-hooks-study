2d9a61c095c0f43637e5a0b1f6914e7362470408
freeman-as <freeman.by2018@gmail.com>
2020-11-15 12:13:39 +0900

    delete all operation logs

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux';
 
 import events from './events';
-import operatoinLogs from './operationLogs';
+import operationLogs from './operationLogs';
 
 export default combineReducers({
   events,
-  operatoinLogs
+  operationLogs
 });
